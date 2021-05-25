d3609e1157cc2d863b1c07d6e777f66a42a75773
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-05-25 14:57:47 +0100

    as

--- addstudent.php
+++ addstudent.php
@@ -1,3 +1,13 @@
+<?php
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+   // check logged in
+   if (isset($_SESSION['id'])) {
+     echo template("templates/partials/header.php");
+     echo template("templates/partials/nav.php");
+
+
 <html>
 
 <h2> Student Details</h2>
@@ -44,6 +54,10 @@ PostCode:
 <input type ="text" name ="txtpostcode" size ="20" class ="form-control"/>
 </br></br>
 
+Password:
+<input name="password" type="password" required/>
+</br></br>
+
 Student Image:
 <input type ="file" name ="studentimage" accept = "image/jpeg" class ="form-control"/>
 </br></br>
@@ -52,3 +66,46 @@ Student Image:
 
 </form>
 </html>
+
+$data['content'] .= '</form>';
+
+if(isset($_POST['btncreate'])){
+  
+  $ID = mysqli_real_escape_string($conn, $_POST['txtID']);
+  $fname = mysqli_real_escape_string($conn, $_POST['txtfname']);
+  $lname = mysqli_real_escape_string($conn, $_POST['txtlname']);
+  $dateofbirth = mysqli_real_escape_string($conn, $_POST['dateofbirth']);
+  $password = mysqli_real_escape_string($conn, $_POST['password']);
+  $firstline = mysqli_real_escape_string($conn, $_POST['txtfirstline']);
+  $town = mysqli_real_escape_string($conn, $_POST['txttown']);
+  $county = mysqli_real_escape_string($conn, $_POST['txtcounty']);
+  $country = mysqli_real_escape_string($conn, $_POST['txtcountry']);
+  $postcode = mysqli_real_escape_string($conn, $_POST['txtpostcode']);
+  
+  $hashedpass = password_hash($password, PASSWORD_DEFAULT);
+  
+  $idcheck = mysqli_query($conn, "SELECT studentid FROM student WHERE studentid = $ID"); //validation query for duplicate student entries.
+  $count = mysqli_num_rows($idcheck);
+  if($count>0)
+  {
+     echo "<H3>Error: Unfortunately, there is already a student with this ID.</H3>";
+  }
+  else
+  {
+    $result = mysqli_query($conn, " INSERT INTO student(studentid,password,dob,firstname,lastname,
+      house,town,county,country,postcode) VALUES
+('$ID','$hashedpass', '$dateofbirth', '$fname', '$lname', '$firstline', '$town', '$county', 
+  '$country', '$postcode');");
+  echo "<H3>Success: A new user has been created!</H3>";
+  }
+  
+}
+   echo template("templates/default.php", $data); 
+}
+else {
+   
+   header("Location: index.php");
+}
+  
+  echo template("templates/partials/footer.php");
+  ?>
--- students.php
+++ students.php
@@ -1,65 +1,75 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-<titile>
-Student Records Table 
-</title>
-
-</head>
-
-<body>
-<form method ="post" action="">
-   <table align = 'center' border ='1' class = "bordered">
-    <thead>
-
-<tr> 
-    <th> <input type = "checkbox" id = "checkAl"> Select all</th>
-    <th>Student ID</th>
-    <th>Password</th>
-    <th>Date of Birth</th>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>House</th>
-    <th>Town</th>
-    <th>County</th>
-    <th>Country</th>
-    <th>Postcode</th>
-    <th>Image</th>
-</tr>
-</thead>
-
+  
 <?php
 
-$u = 0;
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
 
-while($row = mysqli_fetch_array($result))
+// check logged in
+if (isset($_SESSION['id'])) {
+
+   echo template("templates/partials/header.php");
+   echo template("templates/partials/nav.php");
+
+   // Build SQL statment that selects a student's modules
+   $sql = "SELECT studentid,dob,firstname,lastname,house,town,county,country,postcode FROM student;";
+
+   $result = mysqli_query($conn,$sql);
+
+   $data['content'] .= '<form method="post">';
+   $data['content'] .= "<table border='1'>";
+   $data['content'] .= "<tr><th colspan='10' align='center'><h2>Students</h2></th></tr>";
+   $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th>
+   <th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
+
+   // Display the modules within the html table
+   while($row = mysqli_fetch_array($result)) 
+   
+   {
+
+      $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
+      $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>
+      <td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td>
+      <td> $row[postcode] </td> </td> ";
+      $data['content'] .= "<td> <input type ='checkbox' name='delrecords[]' value='".$row['studentid']."' </td></tr> </td>";
+      
+   }
+
+   $data['content'] .= "</table>";
+   $data['content'] .= "</br></br></br>";
+   $data['content'] .= '<input type="submit" name="delete" value="Delete Records">';
+   $data['content'] .= "</form>";
+
+   if(isset($_POST['delete']))
+   
+   {
+
+     $checkboxcount = count($_POST['delrecords']);
+     $i=0;
+     while($i<$checkboxcount)
+
+     {                                 //count of records to delete
+
+       $theid = $_POST['delrecords'][$i];
+       mysqli_query($conn, "DELETE FROM student WHERE studentid= '$theid'");
+       $i++;
+
+     }
+
+      echo "<H3>Success: Data successfully removed!</H3>";
+   }
+
+   // render the template
+   echo template("templates/default.php", $data);
+
+} 
+
+else 
+
 {
- ?>
-    <tr>
-
-      <td align ="center" border ="1"><input type ="checkbox" id ="checkItem" name ="check[]" value ="<?php echo $row ["StudentID"]; ?>"/></td>
-      <td> <?php echo $row["StudentID"]; ?> </td>
-      <td> <?php echo $row["Password"]; ?> </td>
-      <td> <?php echo $row["DOB"]; ?> </td>
-      <td> <?php echo $row["FirstName"]; ?> </td>
-      <td> <?php echo $row["LastName"]; ?> </td>
-      <td> <?php echo $row["House"]; ?> </td>
-      <td> <?php echo $row["Town"]; ?> </td>
-      <td> <?php echo $row["County"]; ?> </td>
-      <td> <?php echo $row["Country"]; ?> </td>
-      <td> <?php echo $row["PostCode"]; ?> </td>
-      <td> <?php echo "<img src = 'studentjpg.php?id =" . $row[id]. "'height ='100'"?></td>
-    
-    </tr>
-
-<?php $u++; 
+   header("Location: index.php");
 }
+
+echo template("templates/partials/footer.php"); 
+mysqli_close($conn);
 ?>
-
-</table>
-<button type ="submit" class ="btn btn-success" name ="save"> Delete</button>
-
-</form>
-</body>
-</html>

4399a0c310ea3bac61c1b42459fe64eb873589fe
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-05-26 15:13:50 +0100

    5 records

--- addstudent.php
+++ addstudent.php
@@ -83,13 +83,65 @@ if(isset($_POST['btncreate'])){
   $country = mysqli_real_escape_string($conn, $_POST['txtcountry']);
   $postcode = mysqli_real_escape_string($conn, $_POST['txtpostcode']);
 
-$sql = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')
-VALUES
-('$studentid', '$password', '$dob', '$fname', '$lname', '$house', '$town', '$county', '$country', '$postcode')";
-$result = mysqli_query($conn, $sql);
-$_POST = null;
+$sql = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905761', '123', '26/12/99', 'Ji-Min', 'Park', '55', 'chesham', 'bucks', 'United Kingdom', 'BP5 2PA';
 
-header("Location: addstudent.php")
+$sql1 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905762', '121', '26/11/98', 'Ho-Seok', 'Jung', '2', 'chesham', 'bucks', 'United Kingdom', 'TP5 2JH)';
+
+$sql2 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905763', '122', '16/11/97', 'Yoon-Gi', 'Min', '3', 'High Wycombe', 'bucks', 'United Kingdom', 'SU5 2GA)';
+
+$sql3 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905732', '124', '16/11/96', 'Taehyung', 'v', '7', 'High Wycombe', 'bucks', 'United Kingdom', 'HP5 2BP)';
+
+$sql4 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905542', '126', '16/1/90', 'Nam-Joon', 'Kim', '1', 'chesham', 'bucks', 'United Kingdom', 'HP5 2RM)';
+
+$sql5 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('2190552', '127', '16/1/93', 'Jung', 'Kook', '5', 'chesham', 'bucks', 'United Kingdom', 'HP5 2JK)';
+
+if ($conn->query($sql1) = TRUE) 
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql1 "</br>" . $conn->error;
+}
+if ($conn->query($sql2) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql2 "</br>" . $conn->error;
+}
+if ($conn->query($sql3) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql3 "</br>" . $conn->error;
+}
+if ($conn->query($sql4) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql4 "</br>" . $conn->error;
+}
+if ($conn->query($sql5) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql5 "</br>" . $conn->error;
+}
+
+
+
+
+
+
+
 
   
   $hashedpass = password_hash($password, PASSWORD_DEFAULT);

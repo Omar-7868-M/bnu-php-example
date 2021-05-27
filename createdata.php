4e4490011e94e79ad7bbab5de31c0646b56f1cc2
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-05-27 14:38:53 +0100

    sa

---
+++ createdata.php
@@ -0,0 +1,56 @@
+<?php
+
+$sql = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905761', '123', '26/12/99', 'Ji-Min', 'Park', '55', 'chesham', 'bucks', 'United Kingdom', 'BP5 2PA';
+
+$sql1 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905762', '121', '26/11/98', 'Ho-Seok', 'Jung', '2', 'chesham', 'bucks', 'United Kingdom', 'TP5 2JH)';
+
+$sql2 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905763', '122', '16/11/97', 'Yoon-Gi', 'Min', '3', 'High Wycombe', 'bucks', 'United Kingdom', 'SU5 2GA)';
+
+$sql3 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905732', '124', '16/11/96', 'Taehyung', 'v', '7', 'High Wycombe', 'bucks', 'United Kingdom', 'HP5 2BP)';
+
+$sql4 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('21905542', '126', '16/1/90', 'Nam-Joon', 'Kim', '1', 'chesham', 'bucks', 'United Kingdom', 'HP5 2RM)';
+
+$sql5 = "INSERT INTO 'student'('studentid', 'password', 'dob', 'firstname', 'lastname', 'house', 'town', 'county', 'country', 'postcode')" VALUES ('2190552', '127', '16/1/93', 'Jung', 'Kook', '5', 'chesham', 'bucks', 'United Kingdom', 'HP5 2JK)';
+
+if ($conn->query($sql1) = TRUE) 
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql1 "</br>" . $conn->error;
+}
+if ($conn->query($sql2) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql2 "</br>" . $conn->error;
+}
+if ($conn->query($sql3) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql3 "</br>" . $conn->error;
+}
+if ($conn->query($sql4) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql4 "</br>" . $conn->error;
+}
+if ($conn->query($sql5) = TRUE)
+{
+  echo "NEW RECORD CREATED SUCCESSFULY"</br>;
+}
+else 
+{
+  echo "ERROR" . $sql5 "</br>" . $conn->error;
+}
+
+?>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
    include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
-   include("addstudent.php");
+   include("createdata.php");
 
    echo template("templates/partials/header.php");
 

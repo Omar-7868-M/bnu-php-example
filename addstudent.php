1b15282049a67d1c4b4c3e842c39a6fc35539da8
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-05-29 14:16:44 +0100

    s

--- addstudent.php
+++ addstudent.php
@@ -1,11 +1,3 @@
-<?php
-   include("_includes/config.inc");
-   include("_includes/dbconnect.inc");
-   include("_includes/functions.inc");
-   // check logged in
-   if (isset($_SESSION['id'])) {
-     echo template("templates/partials/header.php");
-     echo template("templates/partials/nav.php");
 <html>
 
 <h2> Student Details</h2>
@@ -65,6 +57,36 @@ Student Image:
 </form>
 </html>
 
+
+
+
+<?php
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+   // check logged in
+   if (isset($_SESSION['id'])) {
+     echo template("templates/partials/header.php");
+     echo template("templates/partials/nav.php");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
 $data['content'] .= '</form>';
 
 if(isset($_POST['btncreate'])){

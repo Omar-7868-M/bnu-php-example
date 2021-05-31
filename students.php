fbca6f0d7459cff6eb85947efe6a3c1646469cef
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-05-31 18:37:29 +0100

    sa

--- students.php
+++ students.php
@@ -17,13 +17,16 @@ if (isset($_SESSION['id'])) {
    $data['content'] .= '<form method="post">';
    $data['content'] .= "<table border='1'>";
    $data['content'] .= "<tr><th colspan='10' align='center'><h2>Students</h2></th></tr>";
-   $data['content'] .= "<tr><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th>
+   $data['content'] .= "<tr><th>Photo</th><th>Student ID</th><th>DOB</th><th>First Name</th><th>Last Name</th><th>House</th><th>Town</th>
    <th>County</th><th>Country</th><th>Postcode</th><th>Select</th></tr>";
+   
+   
 
    // Display the modules within the html table
    while($row = mysqli_fetch_array($result)) 
    
    {
+      $data['content'] .= "<tr><td><img src='_includes/uploads/$row[picture]'  style='width:50px;height:50px;'></td>"; 
       $data['content'] .= "<tr><td> $row[studentid] </td><td> $row[dob] </td>";
       $data['content'] .= "<td> $row[firstname] </td><td> $row[lastname] </td>
       <td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td>

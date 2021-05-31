24cce30403b084bfdca68b68776841fd7bfec71e
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-05-31 18:17:33 +0100

    sa

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -4,6 +4,8 @@
    <a href="modules.php">My Modules</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
+   <a href="students.php">Students</a>
    <a href="logout.php">Logout</a>
+   
 
 </div>

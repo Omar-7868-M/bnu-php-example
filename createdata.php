0beeb2e5f776a060a5f2b74c4ed77d47f8fc9aed
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-05-31 18:52:32 +0100

    ma

--- createdata.php
+++ createdata.php
@@ -1,16 +1,16 @@
 <?php
 
-$sql1="INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES
-('20000000', '$2y$10$.LJBOl64nZWEVVE/v5mgNuzR01zx1zoyXuGJUa/zp2U.MQxkps3LS', '1974-11-10', 'Jon', 'Smith', '23 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
-$sql2="INSERT INTO`student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES
-('20000001', '$2y$10$.LJBOl64nZWEVVE/v5mgNuzR01zx1zoyXuGJUa/zp2U.MQxkps3Lg', '1974-11-11', 'ali', 'ejaz', '25 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
-$sql3="INSERT INTO`student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES
-('20000002', '$2y$10$.LJBOl64nZWEVVE/v5mgNuzR01zx1zoyXuGJUa/zp2U.MQxkps3Lf', '1974-11-12', 'cristiano', 'ronaldo', '27 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
-$sql4="INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES
-('20000003', '$2y$10$.LJBOl64nZWEVVE/v5mgNuzR01zx1zoyXuGJUa/zp2U.MQxkps3Lz', '1974-11-13', 'raheem', 'Sterling', '29 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
-$sql5="INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES
-('20000004', '$2y$10$.LJBOl64nZWEVVE/v5mgNuzR01zx1zoyXuGJUa/zp2U.MQxkps3Lz', '1974-11-14', 'Lionel', 'Messi', '31 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
+$sql1="INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES('21905762', 'PasswordmapofthesoulBE', '12-9-1994', 'Kim', 'Nam-joon', '23 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
 
+$sql2="INSERT INTO`student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES('21905763', 'PasswordmapofthesoulBE', '04-12-1992', 'Kim', 'Seok-jin', '25 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
+
+$sql3="INSERT INTO`student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES('21905764', 'PasswordmapofthesoulBE', '09-03-1993', 'Min', 'Yoon-gi', '27 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
+
+$sql4="INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES('21905765', 'PasswordmapofthesoulBE', '13-10-1995', 'Park', 'Ji-min', '29 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
+
+$sql5="INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES('21905766', 'PasswordmapofthesoulBE', '30-12-95', 'Kim', 'Tae-hung', '31 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
+
+$sql6="INSERT INTO `student` (`studentid`, `password`, `dob`, `firstname`, `lastname`, `house`, `town`, `county`, `country`, `postcode`) VALUES('21905767', 'PasswordmapofthesoulBE', '01-09-1997', 'Jeon', 'Jung-kook', '31 Victoria Road', 'High Wycombe', 'Bucks', 'UK', 'HP11 1RT');";
 if ($conn->query($sql1) === TRUE){
 	
 	echo "New record created<br/>";
@@ -46,5 +46,11 @@ if ($conn->query($sql5) === TRUE){
 }
 else{
 	echo "Error: " .$sql5."<br/>" . $conn->error;
+}
+if ($conn->query($sql6) === TRUE){
+	echo "New record created<br/>";
+}
+else{
+	echo "Error: " .$sql6."<br/>" . $conn->error;
 }
 ?>

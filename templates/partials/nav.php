17744112cd18cb1e096adf47d9b79773a0269a74
Omar-7868-M <73590999+Omar-7868-M@users.noreply.github.com>
2021-06-01 13:16:55 +0100

    add

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -5,6 +5,7 @@
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
    <a href="students.php">Students</a>
+   <a href="addstudent.php">AddStudents</a>
    <a href="logout.php">Logout</a>
    
 
